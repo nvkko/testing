5d9ccc9a92b4db85ee929f6116e9c3509373212f
nvkko <111242688+nvkko@users.noreply.github.com>
2022-08-14 15:03:17 -0400

    Add files via upload

---
+++ project_data/data.js
@@ -0,0 +1,13 @@
+const data = {};
+
+data.firstTxt = 'The Matrix has you...';
+
+data.secondTxt = 'Follow the White Path...';
+
+data.thirdTxt = '> Learn the Truth <';
+
+data.link = 'https://www.fiverr.com'
+
+data.fontSize = 30;
+
+data.textHeight = 35;
