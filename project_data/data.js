46bd0a51d40b652d09940e57ce390c5126bce09f
nvkko <111242688+nvkko@users.noreply.github.com>
2022-08-15 01:03:27 -0400

    Update data.js

--- project_data/data.js
+++ project_data/data.js
@@ -2,11 +2,11 @@ const data = {};
 
 data.firstTxt = 'You took the First Step...';
 
-data.secondTxt = 'But the journey has just begun...';
+data.secondTxt = 'The journey has just begun...';
 
 data.thirdTxt = '> Experience your Future <';
 
-data.link = 'https://www.nvkko.com'
+data.link = 'www.nvkko.com'
 
 data.fontSize = 30;
 
